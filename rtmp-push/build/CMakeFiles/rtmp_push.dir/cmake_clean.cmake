26f403fbbbd77be61c7ddeacd1df871f28ca6b68
commshare <commshare.zhang@gmail.com>
2015-09-09 15:58:18 +0800

    rtmp-push could not compile success because of bytestream_get_be24

---
+++ rtmp-push/build/CMakeFiles/rtmp_push.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rtmp_push.dir/src/main.cpp.o"
+  "CMakeFiles/rtmp_push.dir/src/CRtmpStream.cpp.o"
+  "rtmp_push.pdb"
+  "rtmp_push"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rtmp_push.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
